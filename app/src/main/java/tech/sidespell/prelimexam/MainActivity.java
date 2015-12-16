d612a3c2035b1eb6628effd98c8e7b8e63fdb78b
Prescious Alago <frederickalago@gmail.com>
2015-12-15 17:44:00 -0800

    Times Up

--- app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
+++ app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
@@ -1,12 +1,27 @@
 package tech.sidespell.prelimexam;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mTextView;
+    private ToggleButton mToggleButton;
+
+    private long timeRemaining = 0;
+    private SeekBar timeSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,27 +30,59 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        timeSpeed = (SeekBar) findViewById(R.id.seekBar);
+
+
+        timeSpeed.setProgress(0);
+        timeSpeed.incrementProgressBy(10);
+        timeSpeed.setMax(1000);
+        TextView seekBarValue = (TextView)layout.findViewById(R.id.seekBar);
+        seekBarValue.setText(tvRadius.getText().toString().trim());
+
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        mToggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+
+        mToggleButton.setOnCheckedChangeListener(this);
+
+
+
+
+        final Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                timeRemaining += 1;
+                mTextView.setText(timeRemaining + "");
+
+                if (timeRemaining > 0) {
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+
+        handler.postDelayed(runnable, 1000);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            Log.d(TAG, "onCheckedChanged: ON");
+            // TODO: Create an alert dialog to ask user input
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            LayoutInflater inflater = LayoutInflater.from(this);
+            View view = inflater.inflate(R.layout.content_main, null);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+             RadioButton userPickInc = (RadioButton) view.findViewById(R.id.radioButton);
+             RadioButton userPickDec = (RadioButton) view.findViewById(R.id.radioButton2);
+
+        } else {
+            Log.d(TAG, "onCheckedChanged: OFF");
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
